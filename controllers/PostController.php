41a85e5debcb22b642fb794eb9ad2f69af7d38e0
Darya Buhtoyarova <daryabuht99@gmail.com>
2022-03-06 22:29:03 +0700

    add pagination and friendly url

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\Post;
+use yii\data\Pagination;
 use yii\web\Controller;
 
 class PostController extends Controller
@@ -10,7 +11,15 @@ class PostController extends Controller
 
     public function actionIndex()
     {
-        $posts = Post::find()-> all();
-        return $this->render('index', compact('posts'));
+       //  $posts = Post::find()-> all();
+$query= Post::find()->with('category');
+$pages = new Pagination(['totalCount'=> $query->count(), 'pageSize'=>4, 'forcePageParam' => false, 'pageSizeParam' => false]);
+$posts = $query->offset($pages->offset)->limit ($pages->limit)->all();
+        return $this->render('index', compact('posts', 'pages'));
+    }
+    public function actionView($id)
+    {
+        var_dump($id);
+        die;
     }
 }
--- views/layouts/blog.php
+++ views/layouts/blog.php
@@ -12,6 +12,7 @@ AppAsset::register($this);
 <html lang="en">
 
 <head>
+    <base href="/">
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
